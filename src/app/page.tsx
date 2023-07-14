bc2fba873a96d4625a1c66379ef977833e49abc6
Ebonian <tanadon.santisan@gmail.com>
2023-07-14 21:44:19 +0700

    feat: init

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useLiff } from "../contexts/LiffContext";
+
+export default function Home() {
+    const liff = useLiff();
+    return <div>liff</div>;
+}
---
+++ src/contexts/LiffContext.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Liff } from "@line/liff/exports";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface LiffContextProps {
+    liff: Liff | null;
+}
+
+export const LiffContext = createContext<LiffContextProps>({
+    liff: null,
+});
+
+export default function LiffProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const [liffObject, setLiffObject] = useState<Liff | null>(null);
+    const [liffError, setLiffError] = useState<string | null>(null);
+
+    useEffect(() => {
+        import("@line/liff")
+            .then((liff) => liff.default)
+            .then((liff) => {
+                console.log("LIFF init...");
+                liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
+                    .then(() => {
+                        console.log("LIFF init succeeded.");
+                        setLiffObject(liff);
+                    })
+                    .catch((error: Error) => {
+                        console.log("LIFF init failed.");
+                        setLiffError(error.toString());
+                    });
+            });
+    }, []);
+
+    return (
+        <LiffContext.Provider
+            value={{
+                liff: liffObject,
+            }}
+        >
+            {children}
+        </LiffContext.Provider>
+    );
+}
+
+export const useLiff = () => {
+    const context = useContext(LiffContext);
+    if (context === undefined) {
+        throw new Error("useLiff must be used within a LiffProvider");
+    }
+    return context.liff;
+};
