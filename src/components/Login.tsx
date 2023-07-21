68ef978ef7f9bb5784362eaef7322835b9096c21
Ebonian <tanadon.santisan@gmail.com>
2023-07-21 21:52:31 +0700

    chore: prevent spam login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -12,6 +12,8 @@ export default function Login({
 
     const isStudentIdValid = /^653\d{5}21$/.test(studentId);
 
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     return (
         <div className="flex flex-col items-center justify-center h-screen w-full text-center space-y-8 pb-6 bg-white">
             <div className="relative w-28 h-28 rounded-full bg-white overflow-hidden border-8 border-white shadow-lg">
@@ -33,7 +35,11 @@ export default function Login({
                 onChange={(e) => setStudentId(e.target.value)}
             />
             <button
-                onClick={() => loginHandler(studentId)}
+                onClick={() => {
+                    if (isLoading) return;
+                    loginHandler(studentId);
+                    setIsLoading(true);
+                }}
                 className={`bg-neutral-800 text-white rounded-lg p-4 text-lg shadow-md duration-300 ${
                     isStudentIdValid
                         ? 'opacity-100 pointer-events-auto'
